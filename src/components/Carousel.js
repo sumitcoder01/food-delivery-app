cc9808059da2d4e026014adb25f7ce62d9677c65
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-07-27 09:04:20 +0530

    update carousel search bar style

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -15,16 +15,15 @@ export default function Carousel({ filter, setFilter }) {
 
     return (
         <div>
-            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner" style={{ zIndex: '-10', height: "450px" }}>
+            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" style={{ height: "450px" }}>
+                <div className="carousel-inner" style={{ zIndex: '-10' , height:"400px" }}>
                     {images.map((image, index) => (
                         <div className={`${index === 0 ? 'carousel-item active' : 'carousel-item'} overflow-y-hidden`} key={index}>
-                            <img src={image} className="w-100 object-fit-contain opacity-50 rounded float-start" alt={`Slide ${index + 1}`} />
+                            <img src={image} className="w-100 object-fit-fill opacity-50 rounded float-start" alt={`Slide ${index + 1}`} />
                         </div>
                     ))}
                 </div>
-
-                <div className="carousel-caption d-none d-md-block text-center" style={{ zIndex: '10' }}>
+                <div className="carousel-caption d-block text-center" style={{ zIndex: '10' }}>
                     <div className="container text-center w-50">
                         <input type="text" className="form-control bg-dark text-white border border-success fst-italic" value={filter} onChange={handleOnChange} style={{ boxShadow: 'none' }} />
                     </div>
